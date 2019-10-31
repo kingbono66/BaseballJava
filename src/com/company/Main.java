a43bb6cb147c8ac62fbd4795329979999a9f5dfa
beaver <beaver@h>
2019-10-31 15:38:01 +0900

    1차 설계

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        ////
+        // 1. 정답을 생성한다
+
+
+        // 2. 추측을 입력받는다
+
+
+        // 3. 결과를 계산한다
+
+
+        // 4. 결과를 출력한다.
+
+
+        // 5. 3s 아니면 2번으로 돌아간다
+
+
+        // 6. 도전횟수를 출력한다
     }
 }
